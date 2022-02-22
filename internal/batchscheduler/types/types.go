efd3e099fb8056e8b86409f388199a76b52485c0
Filipe Regadas <filiperegadas@gmail.com>
2022-02-22 06:29:56 +0000

    fixup! Rework batchscheduler

--- internal/batchscheduler/types/types.go
+++ internal/batchscheduler/types/types.go
@@ -5,18 +5,18 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// BatchScheduler is the general batch scheduler interface.
-type BatchScheduler interface {
-	// Name gets the name of the scheduler
-	Name() string
-	// Schedule reconciles batch scheduling
-	Schedule(options SchedulerOptions, desired *model.DesiredClusterState) error
-}
-
 type SchedulerOptions struct {
 	ClusterName       string
 	ClusterNamespace  string
 	Queue             string
 	PriorityClassName string
 	OwnerReferences   []metav1.OwnerReference
+}
+
+// BatchScheduler is the general batch scheduler interface.
+type BatchScheduler interface {
+	// Name gets the name of the scheduler
+	Name() string
+	// Schedule reconciles batch scheduling
+	Schedule(options SchedulerOptions, desired *model.DesiredClusterState) error
 }
