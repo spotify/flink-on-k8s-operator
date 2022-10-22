2f0f22f74818a0aee56262d86ff73e3e9ddfadd8
Nick Caballero <nick.caballero@offerup.com>
2022-10-21 22:47:47 -0400

    Update Helm chart

--- helm-chart/flink-operator/README.md
+++ helm-chart/flink-operator/README.md
@@ -9,9 +9,6 @@ The instructions to install the Flink operator chart:
 1. Prepare a Flink operator image. You can either use a released image e.g., `ghcr.io/spotify/flink-operator:latest` or follow the instructions [here](https://github.com/spotify/flink-on-k8s-operator/blob/master/docs/developer_guide.md#build-and-push-the-operator-image) to build and push an image from the source code.
 
 2. Run the bash script `update_template.sh` to update the manifest files in templates from the Flink operator source repo (This step is only required if you want to install from the local chart repo).
-    You can set the following env vars to customize the script's behaviour -
-    * `export IMG=<image-name>` - Operator image, defaults to `flink-operator:latest`
-    * `export NS=<namespace-name>` - Namespace to install the operator in, defaults to `flink-operator-system`
 
 3. Register CRD - Don't manually register CRD unless helm install below fails (You can skip this step if your helm version is v3).
 
--- helm-chart/flink-operator/update_template.sh
+++ helm-chart/flink-operator/update_template.sh
@@ -1,17 +1,85 @@
 #!/bin/bash
 
-IMG="${IMG:-flink-operator:latest}"
-NS="${NS:-flink-operator-system}"
+manifests=$(mktemp /tmp/flink-operator-manifests.yaml)
 
-sed -e 's#image: .*#image: '"${IMG}"'#' ../../config/default/manager_image_patch.template >../../config/default/manager_image_patch.yaml
-sed -i '/- \.\.\/crd/d' ../../config/default/kustomization.yaml
-kubectl kustomize ../../config/default | tee templates/flink-operator.yaml
-sed -i '1s/^/{{- if .Values.rbac.create }}\n/' templates/flink-operator.yaml
-sed -i -e "\$a{{- end }}\n" templates/flink-operator.yaml
-sed -i 's/'"${NS}"'/{{ .Values.flinkOperatorNamespace.name }}/g' templates/flink-operator.yaml
-sed -i 's/replicas: 1/replicas: {{ .Values.replicas }}/g' templates/flink-operator.yaml
-sed -i "s/$IMG/{{ .Values.operatorImage.name }}/g" templates/flink-operator.yaml
-sed -i 's/--watch-namespace=/--watch-namespace={{ .Values.watchNamespace.name }}/' templates/flink-operator.yaml
-cp ../../config/crd/bases/flinkoperator.k8s.io_flinkclusters.yaml ../../config/crd/bases
+function yqi() {
+  yq -i "$1" "$manifests"
+}
 
-git checkout ../../config/default/kustomization.yaml
+function modifyManifests() {
+  deploymentSelector='select(.kind == "Deployment")'
+  containersSelector="$deploymentSelector.spec.template.spec.containers"
+  managerSelector="($containersSelector | .[] | select(.name == \"manager\"))"
+  rbacProxySelector="($containersSelector | .[] | select(.name == \"kube-rbac-proxy\"))"
+
+  yqi "$rbacProxySelector"'.image = "__RBAC_PROXY_IMAGE__"'
+  yqi "$rbacProxySelector"'.imagePullPolicy = "__RBAC_PROXY_IMAGE_PULL_POLICY__"'
+  yqi "del($rbacProxySelector.resources)"
+  yqi "$rbacProxySelector |= sort_keys(.)"
+
+  yqi "$managerSelector"'.args += "--watch-namespace={{ .Values.watchNamespace.name }}"'
+  yqi "$managerSelector"'.resources.limits.cpu = "__LIMITS_CPU__"'
+  yqi "$managerSelector"'.resources.limits.memory = "__LIMITS_MEMORY__"'
+  yqi "$managerSelector"'.resources.requests.cpu = "__REQUESTS_CPU__"'
+  yqi "$managerSelector"'.resources.requests.memory = "__REQUESTS_MEMORY__"'
+  yqi "$managerSelector"'.image = "__MANAGER_IMAGE__"'
+  yqi "$managerSelector"'.imagePullPolicy = "__MANAGER_IMAGE_PULL_POLICY__"'
+  yqi "$managerSelector"' |= sort_keys(.)'
+
+  yqi "$containersSelector |= sort_by(.name)"
+  yqi "$managerSelector"'.name = "flink-operator"'
+  yqi "$deploymentSelector"'.spec.template.metadata.annotations["kubectl.kubernetes.io/default-container"] = "flink-operator"'
+
+  yqi "$deploymentSelector"'.spec.replicas = "__REPLICAS__"'
+  yqi "$deploymentSelector"'.spec.template.spec.serviceAccountName = "__SERVICE_ACCOUNT__"'
+  yqi '(select(.kind == "ClusterRoleBinding" or .kind == "RoleBinding").subjects[] | select(.kind == "ServiceAccount")).name = "__SERVICE_ACCOUNT__"'
+  yqi '(select(.kind == "ClusterRoleBinding" or .kind == "RoleBinding").subjects[] | select(.kind == "ServiceAccount")).namespace = "__NAMESPACE__"'
+  yqi 'select(.metadata.namespace != null).metadata.namespace = "__NAMESPACE__"'
+  yqi 'select(.kind == "CustomResourceDefinition").spec.conversion.webhook.clientConfig.service.namespace = "__NAMESPACE__"'
+  yqi 'select(.metadata.annotations["prometheus.io/scrape"] == "true").metadata.name = "__FULL_NAME__-metrics-service"'
+  yqi 'del(.metadata.annotations["cert-manager.io/inject-ca-from"])'
+}
+
+function helmTemplating() {
+  sed 's/__FULL_NAME__/{{ template "flink-operator.fullname" . }}/' |
+  sed 's/__SERVICE_ACCOUNT__/{{ template "flink-operator.serviceAccountName" . }}/' |
+  sed 's/__NAMESPACE__/{{ .Values.flinkOperatorNamespace.name }}/g' |
+  sed 's/__LIMITS_CPU__/{{ .Values.resources.limits.cpu }}/' |
+  sed 's/__LIMITS_MEMORY__/{{ .Values.resources.limits.memory }}/' |
+  sed 's/__REQUESTS_CPU__/{{ .Values.resources.requests.cpu }}/' |
+  sed 's/__REQUESTS_MEMORY__/{{ .Values.resources.requests.memory }}/' |
+  sed 's/__MANAGER_IMAGE_PULL_POLICY__/{{ .Values.operatorImage.pullPolicy }}/' |
+  sed 's/__MANAGER_IMAGE__/{{ .Values.operatorImage.name }}/' |
+  sed 's/__RBAC_PROXY_IMAGE__/{{ .Values.rbacProxyImage.name }}/' |
+  sed 's/__RBAC_PROXY_IMAGE_PULL_POLICY__/{{ .Values.rbacProxyImage.pullPolicy }}/' |
+  sed 's/__REPLICAS__/{{ .Values.replicas }}/g'
+}
+
+function separateManifests() {
+  yq 'select(.apiVersion == "apiextensions.k8s.io/v1")' "$manifests" | helmTemplating > templates/flink-cluster-crd.yaml
+
+  yq 'select(.apiVersion == "rbac.authorization.k8s.io/v1")' "$manifests" | \
+  (echo "{{- if .Values.rbac.create }}" && cat && echo "{{- end }}") | \
+  helmTemplating > templates/rbac.yaml
+
+  read -r -d '' operatorSelector << EOM
+select(true
+and .apiVersion != "apiextensions.k8s.io/v1"
+and .apiVersion != "rbac.authorization.k8s.io/v1"
+and .apiVersion != "cert-manager.io/v1"
+and .apiVersion != "admissionregistration.k8s.io/v1"
+and .kind != "Namespace"
+and .kind != "ServiceAccount"
+)
+EOM
+  yq "$operatorSelector" "$manifests" | helmTemplating > templates/flink-operator.yaml
+}
+
+function main() {
+  kubectl kustomize ../../config/default > "$manifests"
+  modifyManifests
+  separateManifests
+  rm "$manifests"
+}
+
+main
