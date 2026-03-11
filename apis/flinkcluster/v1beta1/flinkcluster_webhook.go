ca5aa038f96584ce03062c4c77ea5fef4f56a178
regadas <oss@regadas.email>
2026-03-11 15:34:41 -0400

    Migrate webhook to generic Defaulter/Validator interfaces
    controller-runtime v0.23.0 changed NewWebhookManagedBy to take the
    object as a second argument and introduced generic Defaulter[T] and
    Validator[T] interfaces that accept concrete types instead of
    runtime.Object.
    
    Update flinkClusterDefaulter and flinkClusterValidator to use
    *FlinkCluster directly, removing the type assertion boilerplate.

--- apis/flinkcluster/v1beta1/flinkcluster_webhook.go
+++ apis/flinkcluster/v1beta1/flinkcluster_webhook.go
@@ -20,10 +20,8 @@ package v1beta1
 import (
 	"context"
 
-	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
-	"sigs.k8s.io/controller-runtime/pkg/webhook"
 	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
 )
 
@@ -31,18 +29,17 @@ import (
 
 var log = logf.Log.WithName("webhook")
 
-// flinkClusterDefaulter implements webhook.CustomDefaulter.
+// flinkClusterDefaulter implements admission.CustomDefaulter for FlinkCluster.
 type flinkClusterDefaulter struct{}
 
-// flinkClusterValidator implements webhook.CustomValidator.
+// flinkClusterValidator implements admission.CustomValidator for FlinkCluster.
 type flinkClusterValidator struct {
 	validator Validator
 }
 
 // SetupWebhookWithManager adds webhook for FlinkCluster.
 func (cluster *FlinkCluster) SetupWebhookWithManager(mgr ctrl.Manager) error {
-	return ctrl.NewWebhookManagedBy(mgr).
-		For(cluster).
+	return ctrl.NewWebhookManagedBy(mgr, cluster).
 		WithDefaulter(&flinkClusterDefaulter{}).
 		WithValidator(&flinkClusterValidator{validator: Validator{}}).
 		Complete()
@@ -56,14 +53,8 @@ The meaning of each marker can be found [here](/reference/markers/webhook.md).
 
 // +kubebuilder:webhook:path=/mutate-flinkoperator-k8s-io-v1beta1-flinkcluster,admissionReviewVersions=v1,sideEffects=None,mutating=true,failurePolicy=fail,groups=flinkoperator.k8s.io,resources=flinkclusters,verbs=create;update,versions=v1beta1,name=mflinkcluster.flinkoperator.k8s.io
 
-var _ webhook.CustomDefaulter = &flinkClusterDefaulter{}
-
-// Default implements webhook.CustomDefaulter.
-func (d *flinkClusterDefaulter) Default(ctx context.Context, obj runtime.Object) error {
-	cluster, ok := obj.(*FlinkCluster)
-	if !ok {
-		return nil
-	}
+// Default implements admission.Defaulter[*FlinkCluster].
+func (d *flinkClusterDefaulter) Default(ctx context.Context, cluster *FlinkCluster) error {
 	log.Info("default", "name", cluster.Name, "original", *cluster)
 	_SetDefault(cluster)
 	log.Info("default", "name", cluster.Name, "augmented", *cluster)
@@ -76,41 +67,23 @@ This marker is responsible for generating a validating webhook manifest.
 
 // +kubebuilder:webhook:path=/validate-flinkoperator-k8s-io-v1beta1-flinkcluster,admissionReviewVersions=v1,sideEffects=None,mutating=false,failurePolicy=fail,groups=flinkoperator.k8s.io,resources=flinkclusters,verbs=create;update,versions=v1beta1,name=vflinkcluster.flinkoperator.k8s.io
 
-var _ webhook.CustomValidator = &flinkClusterValidator{}
 var validator = Validator{}
 
-// ValidateCreate implements webhook.CustomValidator.
-func (v *flinkClusterValidator) ValidateCreate(ctx context.Context, obj runtime.Object) (admission.Warnings, error) {
-	cluster, ok := obj.(*FlinkCluster)
-	if !ok {
-		return nil, nil
-	}
+// ValidateCreate implements admission.Validator[*FlinkCluster].
+func (v *flinkClusterValidator) ValidateCreate(ctx context.Context, cluster *FlinkCluster) (admission.Warnings, error) {
 	log.Info("Validate create", "name", cluster.Name)
 	return nil, v.validator.ValidateCreate(cluster)
 }
 
-// ValidateUpdate implements webhook.CustomValidator.
-func (v *flinkClusterValidator) ValidateUpdate(ctx context.Context, oldObj, newObj runtime.Object) (admission.Warnings, error) {
-	cluster, ok := newObj.(*FlinkCluster)
-	if !ok {
-		return nil, nil
-	}
-	oldCluster, ok := oldObj.(*FlinkCluster)
-	if !ok {
-		return nil, nil
-	}
+// ValidateUpdate implements admission.Validator[*FlinkCluster].
+func (v *flinkClusterValidator) ValidateUpdate(ctx context.Context, oldCluster, cluster *FlinkCluster) (admission.Warnings, error) {
 	log.Info("Validate update", "name", cluster.Name)
 	return nil, v.validator.ValidateUpdate(oldCluster, cluster)
 }
 
-// ValidateDelete implements webhook.CustomValidator.
-func (v *flinkClusterValidator) ValidateDelete(ctx context.Context, obj runtime.Object) (admission.Warnings, error) {
-	cluster, ok := obj.(*FlinkCluster)
-	if !ok {
-		return nil, nil
-	}
+// ValidateDelete implements admission.Validator[*FlinkCluster].
+func (v *flinkClusterValidator) ValidateDelete(ctx context.Context, cluster *FlinkCluster) (admission.Warnings, error) {
 	log.Info("validate delete", "name", cluster.Name)
-	_ = cluster
 	// TODO
 	return nil, nil
 }
